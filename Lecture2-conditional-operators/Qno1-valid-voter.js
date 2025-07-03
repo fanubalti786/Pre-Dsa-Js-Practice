0f95824bd33c36042a82a248896733156dbee9f3
fanubalti786 <fanubalti786@gmail.com>
2025-07-03 14:16:01 +0600

    shop discount

--- Lecture2-conditional-operators/Qno1-valid-voter.js
+++
@@ -1,8 +0,0 @@
- let age = Number(prompt("Enger your Age: "));
-        if(age >= 18)
-            {
-                console.log("you can vote");
-            }
-        else{
-            console.log("you can't vote");
-        }
