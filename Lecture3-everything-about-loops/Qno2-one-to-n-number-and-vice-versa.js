5cdac3bd826f36f2e9e3c826b1b31902228345e9
fanubalti786 <fanubalti786@gmail.com>
2025-07-04 17:41:35 +0600

    print specific number and its vice versa

---
+++ Lecture3-everything-about-loops/Qno2-one-to-n-number-and-vice-versa.js
@@ -0,0 +1,14 @@
+// 1 to 10
+for(let i = 1; i<=10; i++)
+{
+    console.log("the value of i : " + i);
+    
+}
+
+
+console.log("\n");
+// 10 to 1
+for(let i = 10; i>=1; i--)
+{
+    console.log("the value of i: " + i)
+}
