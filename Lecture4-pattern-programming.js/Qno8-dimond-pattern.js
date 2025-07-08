e48bc257abb02d59a9a94c4bf7e8e33c403ec76a
fanubalti786 <fanubalti786@gmail.com>
2025-07-08 21:18:18 +0600

    dimond pattern

---
+++ Lecture4-pattern-programming.js/Qno8-dimond-pattern.js
@@ -0,0 +1,12 @@
+let n = 6;
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    process.stdout.write("-");
+  }
+  for(let k = 1; k <= 2*i-1; k++ )
+  {
+    process.stdout.write("*");
+  }
+  console.log("");
+}
