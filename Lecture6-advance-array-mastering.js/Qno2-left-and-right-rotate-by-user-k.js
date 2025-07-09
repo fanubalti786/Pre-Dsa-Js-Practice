32a201081882b32b8855c6f02022962455e20937
fanubalti786 <fanubalti786@gmail.com>
2025-07-09 14:24:09 +0600

    left and right rotation with user input k

--- Lecture6-advance-array-mastering.js/Qno2-left-and-right-rotate-by-user-k.js
+++ Lecture6-advance-array-mastering.js/Qno2-left-and-right-rotate-by-user-k.js
@@ -1,15 +1,16 @@
 // left rotation
 let prompt = require("prompt-sync")();
-let k = +prompt("Enter the value of k: ");
 let arr = [1, 2, 3, 4, 5, 6];
+let k = +prompt("Enter the value of k: ");
+k = k % arr.length;
 
-let fval = arr[0];
+for (let j = 0; j < k; j++) {
+  let fval = arr[0];
   for (let i = 0; i < arr.length - 1; i++) {
     arr[i] = arr[i + 1];
   }
-
-
-arr[arr.length - 1] = fval;
+  arr[arr.length - 1] = fval;
+}
 
 console.log(arr);
 
@@ -17,11 +18,13 @@ console.log(arr);
 
 let arr1 = [1, 2, 3, 4, 5, 6];
 
-let fval1 = arr1[arr1.length - 1];
+for (let j = k; j >= 1; j--) {
+  let fval1 = arr1[arr1.length - 1];
+
   for (let i = arr1.length - 1; i > 0; i--) {
     arr1[i] = arr1[i - 1];
   }
+  arr1[0] = fval1;
+}
 
-
-arr1[0] = fval1;
 console.log(arr1);
