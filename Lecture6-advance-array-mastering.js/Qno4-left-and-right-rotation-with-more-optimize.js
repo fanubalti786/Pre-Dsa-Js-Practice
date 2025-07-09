fd327f988725ab5fae98e92c4e58bc7be0e50f56
fanubalti786 <fanubalti786@gmail.com>
2025-07-10 03:57:34 +0600

    more perfect solution with high optimization left and right rotation with k space

--- Lecture6-advance-array-mastering.js/Qno4-left-and-right-rotation-with-more-optimize.js
+++ Lecture6-advance-array-mastering.js/Qno4-left-and-right-rotation-with-more-optimize.js
@@ -5,8 +5,14 @@ k = k % arr.length;
 reverse(0, k-1);
 reverse(k, arr.length-1);
 reverse(0, arr.length-1);
-console.log(arr);
+console.log(`with left side ${k} spacing ${arr}`);
 
+
+arr = [1,2,3,4,5];
+reverse(0, arr.length-1);
+reverse(0, k-1);
+reverse(k, arr.length-1);
+console.log(`with right side ${k} spacing ${arr}`);
 function reverse(i , j){
     while(i<j)
     {
