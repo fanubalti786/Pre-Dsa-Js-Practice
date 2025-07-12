d09ec475df0857fe15395b25a8fa359c93c58ee8
fanubalti786 <fanubalti786@gmail.com>
2025-07-12 23:49:34 +0500

    best time to sell and buy stocks leadcode Question solved alhamdulillah

---
+++ Lecture6-advance-array-mastering.js/Qno8-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+let stockPrice = [7,1,5,3,6,4];
+let profit = 0;
+let min = stockPrice[0];
+
+for(let i = 0; i<=stockPrice.length; i++)
+{
+    if(stockPrice[i] < min)
+    {
+        min = stockPrice[i];
+    }else if(stockPrice[i] > min) {
+        let put = stockPrice[i] - min ;
+        if(put > profit)
+        {
+            profit = stockPrice[i];
+        }
+    }
+}
+
+console.log(profit);
